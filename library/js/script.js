8cb3a913470219601a08249e2d0b2397cec79899
Maxim Pachuzheuski <makspohp123@gmail.com>
2023-08-07 23:49:19 +0300

    feat:Added js work to burger menu

--- library/js/script.js
+++ library/js/script.js
@@ -1,9 +1,32 @@
-const burger = document?.querySelector('[data-burger]');
-const nav = document?.querySelector('[data-nav]');
-const body = document.body;
+const burger = document?.querySelector("[data-burger]");
+const nav = document?.querySelector("[data-nav]");
+const navigation = nav?.querySelectorAll("a");
 
-burger?.addEventListener('click' , () => {
-    body.classList.toggle('stop-scroll')
-   burger?.classList.toggle('burger--active')
-   nav.classList.toggle('navbutton--visibl'); 
-})
+burger?.addEventListener("click" , () => {
+   burger?.classList.toggle("burger--active");
+   nav?.classList.toggle("navbutton--visibl") ;
+});
+
+navigation.forEach(el => {
+    el.addEventListener("click", () => {
+        burger?.classList.remove("burger--active");
+        nav?.classList.remove("navbutton--visibl");   
+    })
+});
+
+
+window.addEventListener ("keydown", function(event){
+    if (event.keyCode === 27){
+        burger?.classList.remove("burger--active");
+        nav?.classList.remove("navbutton--visibl");   
+    }
+  })
+
+window.addEventListener("click", function(event){
+    if (!event.target.classList.contains("burger--active") && !event.target.closest(".navbutton")) {
+      burger?.classList.remove("burger--active");
+      nav?.classList.remove("navbutton--visibl");    
+    } 
+  });
+
+ 
